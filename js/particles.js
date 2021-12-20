9abe0930e42720e584ce96c064ccc10942a3f6f1
Cameron McGarry <cameron@cmcgarry.co.uk>
2021-12-20 00:16:47 +0000

    Configure potfolio and particles to play nice

--- js/particles.js
+++ js/particles.js
@@ -13,39 +13,50 @@ for (var i=0; i<n; i++) {
   vs.push(v);
 }
 
+var layout = {
+   margin: {
+    l: 0,
+      r: 0,
+    b: 0,
+    t: 0
+  },
+  autosize: true,
+  hovermode: false,
+  xaxis: {
+    range: [-.15, .15],
+    showgrid: false,
+    zeroline: false,
+    visible: false,
+    fixedrange: true
+  },
+  yaxis: {
+    range: [-2, 2],
+    showgrid: false,
+    zeroline: false,
+    visible: false,
+    fixedrange: true
+  },
+};
+
+var config = {
+  displayModeBar: false
+}
+
 Plotly.newPlot('simulation', [{
   x: xs,
   y: vs,
   mode: 'markers',
   marker: {
-    //color: 'rgb(17, 157, 255)',
     size: 3,
-//    line: {
-//      color: 'rgb(231, 99, 250)',
-//      width: 0}
     },
-}], {
-  hovermode: false,
-  xaxis: {
-    range: [-.22, .22],
-    'showgrid': false,
-    'zeroline': false,
-    'visible': false,
-    'fixedrange': true
-  },
-  yaxis: {
-    range: [-2.2, 2.2],
-    'showgrid': false,
-    'zeroline': false,
-    'visible': false,
-    'fixedrange': true
-  },
-}, {'displayModeBar': false})
+}],
+layout,
+config
+)
 
 function compute () {
   var dx, dv;
   var xh, yh, zh;
-//  var dt = 0.0015;
   var dt = 0.0002;
 
   for (var i = 0; i < n; i++) {
@@ -73,10 +84,9 @@ function update () {
       duration: 0,
       redraw: false
     }
-  });
+  }, layout);
 
   requestAnimationFrame(update);
 }
 
 requestAnimationFrame(update);
-
